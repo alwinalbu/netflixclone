c9410893dd7acd28b419c9ef668c66c77352a4cf
alwinalbu <alwin.009@gmail.com>
2023-08-10 17:51:54 +0530

    first

---
+++ script.js
@@ -0,0 +1,85 @@
+const btn1 = document.getElementById("first1");
+const para = document.getElementById("para");
+
+btn1.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para).display;
+    if(ele=='none'){
+        para.style.display='block';
+    }
+    else if(ele=='block'){
+        para.style.display='none';
+    }
+    
+});
+
+const btn2 = document.getElementById("first2");
+const para2 = document.getElementById("para2");
+
+btn2.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para2).display;
+    if(ele=='none'){
+        para2.style.display='block';
+    }
+    else if(ele=='block'){
+        para2.style.display='none';
+    }
+    
+});
+const btn3 = document.getElementById("first3");
+const para3 = document.getElementById("para3");
+
+btn3.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para3).display;
+    if(ele=='none'){
+        para3.style.display='block';
+    }
+    else if(ele=='block'){
+        para3.style.display='none';
+    }
+    
+});
+const btn4 = document.getElementById("first4");
+const para4 = document.getElementById("para4");
+
+btn4.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para4).display;
+    if(ele=='none'){
+        para4.style.display='block';
+    }
+    else if(ele=='block'){
+        para4.style.display='none';
+    }
+    
+});
+const btn5 = document.getElementById("first5");
+const para5 = document.getElementById("para5");
+
+btn5.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para5).display;
+    if(ele=='none'){
+        para5.style.display='block';
+    }
+    else if(ele=='block'){
+        para5.style.display='none';
+    }
+    
+});
+const btn6 = document.getElementById("first6");
+const para6 = document.getElementById("para6");
+
+btn6.addEventListener('click', function()
+{
+    var ele=window.getComputedStyle(para6).display;
+    if(ele=='none'){
+        para6.style.display='block';
+    }
+    else if(ele=='block'){
+        para6.style.display='none';
+    }
+    
+});
